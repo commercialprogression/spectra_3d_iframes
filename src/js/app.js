a5e180ce0c8eae85435e0eff7494bcff478fe397
Hillary Lewandowski <hillary@commercialprogression.com>
2019-01-12 16:05:53 -0500

    Turn down the light source to reduce shininess

--- src/js/app.js
+++ src/js/app.js
@@ -26,13 +26,13 @@ threeDObjects.forEach(function(tDObject, index){
   threes.lighting = true;
   threes.ambient = new THREE.AmbientLight(0xffffff, 0.75);
   threes.scene.add(threes.ambient);
-  threes.keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 0.35);
+  threes.keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(30, 100%, 75%)'), 0.05);
   threes.keyLight.position.set(-100, 100, 100).normalize();
   threes.scene.add(threes.keyLight);
-  threes.fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.25);
+  threes.fillLight = new THREE.DirectionalLight(new THREE.Color('hsl(240, 100%, 75%)'), 0.05);
   threes.fillLight.position.set(100, -25, 100).normalize();
   threes.scene.add(threes.fillLight);
-  threes.backLight = new THREE.DirectionalLight(0xffffff, 0.25);
+  threes.backLight = new THREE.DirectionalLight(0xffffff, 0.05);
   threes.backLight.position.set(100, 0, -100).normalize();
   threes.scene.add(threes.backLight);
 
